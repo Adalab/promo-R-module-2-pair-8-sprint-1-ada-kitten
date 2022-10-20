e1a54078408c5c92765f0868540a68064fad3fa1
Yara <yara.gordillo@gmail.com>
2022-10-20 11:59:54 +0200

    some filter ideas

--- src/js/main.js
+++ src/js/main.js
@@ -54,10 +54,12 @@ const Kitten3 =
   // '<li class="card"><img class="card_img"src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"alt="gatito" /><h3 class="card_title">Cielo</h3><h4 class="card_race">British Shorthair</h4><p class="card_description">Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!</p></li>';
   `<li class="card"><img class="card_img" src="${KittenImg3}" alt="gatito" /><h3 class="card_title">${KittenName3UC}</h3><h4 class="card_race">${KittenRace3}</h4><p class="card_description">${KittenDesc3}</p></li>`;
 
-const kittens = document.getElementsByClassName('card_description');
+const kittensDescs = document.querySelectorAll('.card_description');
+for (const card_description of kittensDescs) {
+  card_description.classList.add('collapsed');
+}
 
 if (KittenDesc1.includes(descrSearchText)) {
-  kittens.classList.add('hidden');
 }
 
 NewKitten.innerHTML = Kitten1 + Kitten2 + Kitten3;
